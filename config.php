f0c0abe9e842fd5221e11accd8c8e78c6f036e50
HenriqueCJ <128942204+HenriqueCarradoreJoaquim@users.noreply.github.com>
2024-09-26 20:10:31 -0300

    Update config.php

--- config.php
+++ config.php
@@ -7,12 +7,4 @@
 
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
-    /*if($conexao->connect_errno)
-    {
-        echo "erro";
-    }
-    else
-    {
-        echo "conexao efetuada com sucesso"; 
-    }*/
 ?>
