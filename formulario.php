8ef45bf75330fd107a490ae84987982962ebceda
HenriqueCJ <128942204+henrique2806@users.noreply.github.com>
2024-09-26 15:27:58 -0300

    Update formulario.php

--- formulario.php
+++ formulario.php
@@ -1,21 +1,6 @@
 <?php
 if (isset($_POST['submit'])) {
-    /*print_r('Nome: ' . $_POST['nome']);
-        print_r('<br>');
-        print_r('E-mail: ' . $_POST['email']);
-        print_r('<br>');
-        print_r('Telefone: ' . $_POST['telefone']);
-        print_r('<br>');
-        print_r('sexo: ' . $_POST['genero']);
-        print_r('<br>');
-        print_r('Data de Nascimento: ' . $_POST['data_nascimento']);
-        print_r('<br>');
-        print_r('Cidade: ' . $_POST['cidade']);
-        print_r('<br>');
-        print_r('Estado: ' . $_POST['estado']);
-        print_r('<br>');
-        print_r('Endereço: ' . $_POST['endereco']);*/
-
+    
     include_once('config.php');
 
     $nome = $_POST['nome'];
